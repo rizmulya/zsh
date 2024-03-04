a7981a624b99ba2c712cf170c67514773bff8a5a
rizmulya <riskymulya@protonmail.com>
2024-03-04 21:47:24 +0700

    fix

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
+#!/bin/bash
 if [[ $EUID -ne 0 ]]; then
     echo "This script requires root privileges."
     exit 1
 fi
 
-apt install zsh && \
-    zsh && \
-    chsh -s /bin/bash $1 && \
-    su - && \
-    chsh -s /bin/bash root
+apt install zsh
+zsh
+chsh -s /bin/zsh $1
+su -
+chsh -s /bin/zsh root
 
-su $1 && \
-    cp .zshrc ~
-sudo su && \
-    cp .zshrc ~
-    cp zsh/* /usr/share -r
+su $1
+cp .zshrc ~
+sudo su
+cp .zshrc ~
+cp zsh/* /usr/share -r
