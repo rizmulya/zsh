80608457cf024f99a015c95b0379847a78d321ae
Risky Mulya <122626344+rizmulya@users.noreply.github.com>
2024-03-06 18:09:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
+
 if [[ $EUID -ne 0 ]]; then
     echo "This script requires root privileges."
     exit 1
 fi
 
-chsh -s /bin/zsh $1
-chsh -s /bin/zsh root
-cp .zshrc ~
-cp zsh/* /usr/share -r
+if [ -z "$1" ]; then
+    echo "Usage: $0 <username>"
+    exit 1
+fi
 
-su $1
-cp .zshrc ~
+cp -r zsh/* /usr/share/
+echo "Copied Zsh configuration to /usr/share"
+
+chsh -s /bin/zsh root
+cp .zshrc /root
+echo "Changed default shell to Zsh for root."
+
+chsh -s /bin/zsh $1
+if [[ -n $1 ]]; then
+    HOME_DIR=$(eval echo ~$1)
+    cp .zshrc "$HOME_DIR"
+    chown $1: "$HOME_DIR/.zshrc"
+fi
+echo "Changed default shell to Zsh for user: $1"
+echo "--------- everythings good -----------"
